e9d4190a7a8e4a9e90b7be64202999d0326293a8
IamGreedy <pl2909.couperin@gmail.com>
2025-09-16 14:42:24 +0200

    2:FPS

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
@@ -13,6 +15,8 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        time_passed = clock.tick(60)
+        dt = time_passed/1000
 
 if __name__ == "__main__":
     main()
